017c2b534be14bcff9167bbd5c5614a4251935fa
Caweca105 <lemos.diogo105@gmail.com>
2021-11-08 16:15:23 +0000

    Update 404.js
    Resolved a render problem that my genius brain couldn't find

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,6 @@
 import NextLink from 'next/link'
-import {
+import { 
     Box,
-    Heading,
     Text,
     Container,
     Divider,
@@ -9,17 +8,19 @@ import {
 } from '@chakra-ui/react'
 
 const NotFound = () => {
-    <Container>
-        <Heading as="h1">Not found</Heading>
-        <Text>The page you&apos;re looking for was not found</Text>
-        <Divider my={6} />
+    return (
+        <Container>
+            <Heading as="h1">Not found</Heading>
+            <Text>The page you&apos;re looking for was not found</Text>
+            <Divider my={6} />
 
-        <Box my={6} align="center">
-            <NextLink href="/">
-                <Button colorScheme="teal">Return to home</Button>
-            </NextLink>
-        </Box>
-    </Container>
+            <Box my={6} align="center">
+                <NextLink href="/">
+                    <Button colorScheme="teal">Return to home</Button>
+                </NextLink>
+            </Box>
+        </Container>
+    )
 }
 
 export default NotFound

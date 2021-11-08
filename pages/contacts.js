2d71f1d32d46b3814c5085488293c0569529255c
Caweca105 <lemos.diogo105@gmail.com>
2021-11-08 18:29:35 +0000

    Update contacts.js
    Updated all boxes from contact form

--- pages/contacts.js
+++ pages/contacts.js
@@ -44,21 +44,21 @@ export default function Form() {
               <form name="contact" method="POST" action="/?success=true" data-netlify="true">
                 <input type="hidden" name="form-name" value="contact" />
 
-                <Box borderRadius="lg" bg={useColorModeValue('#94d2bd', 'whiteAlpha.200')} p={3} mb={6} align="center">
+                <Box borderRadius="lg" bg={useColorModeValue('#94d2bd', '#008080')} p={3} mb={6} align="center">
                   <FormControl id="name" isRequired>
                     <FormLabel>Name</FormLabel>
                     <Input />
                   </FormControl>
                 </Box>
 
-                <Box borderRadius="lg" bg={useColorModeValue('#94d2bd', 'whiteAlpha.200')} p={3} mb={6} align="center">
+                <Box borderRadius="lg" bg={useColorModeValue('#94d2bd', '#008080')} p={3} mb={6} align="center">
                   <FormControl id="Company">
                     <FormLabel>Company</FormLabel>
                     <Input />
                   </FormControl>
                 </Box>
 
-                <Box borderRadius="lg" bg={useColorModeValue('#94d2bd', 'whiteAlpha.200')} p={3} mb={6} align="center">
+                <Box borderRadius="lg" bg={useColorModeValue('#94d2bd', '#008080')} p={3} mb={6} align="center">
                   <FormControl id="email" isRequired>
                     <FormLabel>Email adress</FormLabel>
                     <Input 
@@ -69,7 +69,7 @@ export default function Form() {
                   </FormControl>
                 </Box>
 
-                <Box borderRadius="lg" bg={useColorModeValue('#94d2bd', 'whiteAlpha.200')} p={3} mb={6} align="center">
+                <Box borderRadius="lg" bg={useColorModeValue('#94d2bd', '#008080')} p={3} mb={6} align="center">
                   <FormControl id="name" isRequired>
                     <FormLabel>Message</FormLabel>
                     <Textarea 

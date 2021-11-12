068d7a1ab2e62924565a06c53b1ca3f7815896f6
Caweca105 <lemos.diogo105@gmail.com>
2021-11-12 16:41:49 +0000

    Update contacts.js

--- pages/contacts.js
+++ pages/contacts.js
@@ -25,12 +25,6 @@ export default function Form() {
     }
   }, [])
 
-  function encode(data) {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&")
-  }
-
   return (
     <Layout>
       <Section delay={0.1}>

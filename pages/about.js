7242548bcb5b23c2d9205f0164ef78c214b1b4d6
Caweca105 <lemos.diogo105@gmail.com>
2021-11-15 19:08:11 +0000

    Update about.js
    Added a thank you note, added some paragraphs

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,15 @@
 import {
   Container,
   Heading,
+  Button,
+  Icon,
   Box,
+  Link,
 } from '@chakra-ui/react'
+import {
+    IoLogoInstagram,
+    IoLogoLinkedin
+  } from 'react-icons/io5'
 import Paragraph from '../components/paragraph'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
@@ -57,8 +64,14 @@ const About = () => (
 
                     <Paragraph>
                         For 10 weeks I took part in the intensive programming Boot Camp of Bytes4Future. In this program I learned to write JavaScript (ES6) code, I also learned the basis of topics such as Object Oriented Programming, Data Structures,Algorithms, Functional Programming and Basic Software Principles.
+                    </Paragraph>
+
+                    <Paragraph>
                         Starting from the HTML and CSS base acquired in the PowerUp stage of Bytes4Future, I learned to develop interfaces using the declarative model, more specifically, using React. I also learned what Redux is all about.
                         I learned how to use JavaScript in the back end using NodeJS to run JavaScript code outside the browser. I developed REST APIs using Express and had my first chance to play with Web Sockets.
+                    </Paragraph>
+
+                    <Paragraph>
                         To store application data, I learned how to use files, most of the time to store JSON objects. I also learned to work with MongoDB and learned the basics of SQL.
                         During the development of projects I used Git and GitHub to manage source code versions.
                     </Paragraph>
@@ -76,7 +89,48 @@ const About = () => (
                     </Paragraph>
                 </Section>
 
+                <Section delay={1.7}>
+                    <Heading as="h3" variant="section-title">
+                        Thank you
+                    </Heading>
 
+                    <Paragraph>
+                        I want to thank to my dear friend Fernanda, who made the 3D model of Vaporeon at my request. 
+                        <Link href="https://www.instagram.com/lomiartwork/" target="_blank">
+                        <Button
+                            variant="ghost"
+                            colorScheme="teal"
+                            leftIcon={<Icon as={IoLogoInstagram} />}
+                        >
+                            @lomiartwork
+                        </Button>
+                        </Link>
+                    </Paragraph>
+
+                    <Paragraph>
+                        I would also like to thank 
+                        <Link href="https://www.linkedin.com/in/daniel-barreiras/" target="_blank">
+                        <Button
+                            variant="ghost"
+                            colorScheme="teal"
+                            leftIcon={<Icon as={IoLogoLinkedin} />}
+                        >
+                            Daniel Barradas
+                        </Button>
+                        </Link>
+                         and 
+                         <Link href="https://www.linkedin.com/in/ricardojjustino/" target="_blank">
+                        <Button
+                            variant="ghost"
+                            colorScheme="teal"
+                            leftIcon={<Icon as={IoLogoLinkedin} />}
+                        >
+                            Ricardo Justino
+                        </Button>
+                        </Link> 
+                        for their valuable inputs, that made me improve my site!
+                    </Paragraph>
+                </Section>
             </Box>
         </Container>
     </Layout>
